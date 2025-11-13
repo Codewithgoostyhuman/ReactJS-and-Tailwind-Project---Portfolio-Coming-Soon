66a34d4fad61077fb467acbb9223af265c62e965
Codewithgoostyhuman <zeeshantahiralvi123@gmail.com>
2025-11-13 22:04:54 +0500

    another commit for mobile

--- src/components/Hero-Subcomponents/AboutMe.jsx
+++ src/components/Hero-Subcomponents/AboutMe.jsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
   return (
     <div className='flex flex-col justify-center align-middle items-center w-fit h-fit lg:p-10 md:p-7 sm:p-5 p-3 ml-auto mr-auto'>
       <h2 className='font-extrabold lg:text-3xl md:text-2xl sm:text-xl text-md lg:mb-8 md:mb-8 sm:mb-6 mb-4 text-white bg-orange-700 w-fit lg:p-6 md:p-4 sm:p-2 p-2 lg:rounded-b-4xl md:rounded-b-3xl sm:rounded-b-2xl rounded-b-2xl border-b-2 border-gray-600 hover:bg-gray-300 hover:text-gray-800 hover:border-gray-800 hover:border-b-2 transition-all ease-in-out duration-1000'>COMING SOON</h2>
-      <p className='wrap-break-word lg:p-4 md:p-3 sm:p-2 p-2 lg:text-2xl md:text-xl sm:text-lg text-sm  font-bold'>STAY TUNED FOR MY NEW PORTFOLIO!</p>
+      <p className='wrap-break-word lg:p-4 md:p-3 sm:p-2 p-2 lg:text-2xl md:text-xl sm:text-lg text-sm justify-center align-middle  font-bold'>STAY TUNED FOR MY NEW PORTFOLIO!</p>
     </div>
   )
 }
